1f96f36b4505d0c6412fb64713f23b9a21ddf9b2
swparabellum <swparabellum@gmail.com>
2025-07-03 21:00:39 +0900

    3.7.2 뷰 함수 index() 및 템플릿 작성

---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+
+    path('', views.index, name='index'), # /polls/
+    path('<int:question_id>/', views.detail, name='detail'), # /polls/1/
+    path('<int:question_id>/results/', views.results, name='results'), # /polls/1/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'), # /polls/1/vote/
+
+]
